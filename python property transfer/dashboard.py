531930b577d8c2c62527ddd3ae1d943b729c2ee7
Argel <107836860+MarkMalihan@users.noreply.github.com>
2023-05-16 09:47:45 +0800

    Add files via upload
    Footer added
    Date filter button added
    some small changes on mini dashboard

--- python property transfer/dashboard.py
+++ python property transfer/dashboard.py
@@ -3,6 +3,7 @@ from tkinter import ttk
 import customtkinter as ctk
 from tkinter.messagebox import showinfo
 from PIL import Image
+import tkcalendar
 
 ctk.set_appearance_mode("system")  # Modes: system (default), light, dark
 
@@ -11,7 +12,7 @@ class history_window(ctk.CTkToplevel):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.title("Property Transfer | History")
-        self.geometry("{0}x{1}+200+50".format(1000, 600))
+        self.geometry("{0}x{1}+200+50".format(950, 550))
         self.resizable(False, False)
         self.configure(fg_color = 'white')
         self.iconbitmap('python property transfer\images\icons8-data-transfer-483.ico')
@@ -224,14 +225,21 @@ class items_window(ctk.CTkToplevel):
         #notification bell
         notif_bell_icon = ctk.CTkImage(Image.open('python property transfer\images\icons8-bell-48.png'), size=(30, 30))
         notif_bell_button = ctk.CTkButton(header_frame, image= notif_bell_icon, text=None, fg_color='transparent', width= 30)
-        notif_bell_button.pack(side = 'right', pady = 10, padx = 20)        
+        notif_bell_button.pack(side = 'right', pady = 10, padx = 20)
+        
+        frame = ctk.CTkFrame(self, fg_color='transparent')
+        frame.pack(fill = 'both')
+        label = ctk.CTkLabel(frame, text = 'Items', font= ctk.CTkFont('Arial', size = 22, weight = "bold"), text_color="black")
+        label.pack(side = 'left',  pady = 15, padx = 15)
+        
+                
 
 #dashboard window
 class dashboard_window(ctk.CTk):
     def __init__(self):
         super().__init__()
         self.title("Property Transfer | Dashboard")
-        self.geometry("{0}x{1}+200+50".format(1000, 600))
+        self.geometry("{0}x{1}+200+50".format(1000,550))
         self.resizable(False, False)
         self.configure(fg_color = 'white')
         self.iconbitmap('python property transfer\images\icons8-data-transfer-483.ico')
@@ -266,8 +274,12 @@ class dashboard_window(ctk.CTk):
         notif_bell_button = ctk.CTkButton(header_frame, image= notif_bell_icon, text=None, fg_color='transparent', width= 30)
         notif_bell_button.pack(side = 'right', pady = 10, padx = 20)
         
+        #footer
+        footer_frame = ctk.CTkFrame(self, fg_color='maroon', height=26, corner_radius=0)
+        footer_frame.pack(fill = 'both', side='bottom')
+        
         #mini dashboard frame
-        mini_dashboard_frame = ctk.CTkFrame(self, fg_color='transparent' )
+        mini_dashboard_frame = ctk.CTkFrame(self, fg_color='transparent')
         mini_dashboard_frame.pack(fill='both')
         
         #mini dashboard
@@ -277,29 +289,36 @@ class dashboard_window(ctk.CTk):
         mini_dashboard_container = ctk.CTkFrame(self, fg_color='transparent')
         mini_dashboard_container.pack(fill = 'both')
         
+        date_filter_icon = ctk.CTkImage(Image.open('python property transfer\images\icons8-date-100.png'), size=(40, 40))
+        date_filter = ctk.CTkButton(mini_dashboard_frame, text =None, image=date_filter_icon,fg_color='transparent', width= 30 )
+        date_filter.pack(side = 'right', pady = 15, padx = 15)
+        
         #mini dashboard contents
         mini_dashboard_content_frame = ctk.CTkFrame(mini_dashboard_container, fg_color = 'transparent')
-        mini_dashboard_content_frame.pack(side = 'left', padx =20,fill = 'y')
+        mini_dashboard_content_frame.pack(side = 'left', padx =24, fill = 'y')
         
         content1 = ctk.CTkFrame(mini_dashboard_content_frame, height=100, width=285, corner_radius=20)
-        content1.pack(fill = 'y')
-        content1_label = ctk.CTkLabel(content1, text="No. of Items", height=100, width=285, anchor="nw", font= ctk.CTkFont('Arial', size = 22, weight = "bold"))
+        content1.pack()
+        content1.pack_propagate(0)
+        content1_label = ctk.CTkLabel(content1, text="No. of Items", height=80, width=260, anchor="nw", font= ctk.CTkFont('Arial', size = 22, weight = "bold"))
         content1_label.pack(pady = (10,0), padx = (10,0))
         
         mini_dashboard_content_frame2 = ctk.CTkFrame(mini_dashboard_container, fg_color = 'transparent')
-        mini_dashboard_content_frame2.pack(side = 'left', padx =20, fill = 'y')
+        mini_dashboard_content_frame2.pack(side = 'left', padx =24, fill = 'y')
         
         content2 = ctk.CTkFrame(mini_dashboard_content_frame2, height=100, width=285, corner_radius=20 )
-        content2.pack(fill = 'y')
-        content2_label = ctk.CTkLabel(content2, text="No. of Transfers", height=100, width=285, anchor="nw", font= ctk.CTkFont('Arial', size = 22, weight = "bold"))
+        content2.pack()
+        content2.pack_propagate(0)
+        content2_label = ctk.CTkLabel(content2, text="No. of Transfers", height=80, width=260, anchor="nw", font= ctk.CTkFont('Arial', size = 22, weight = "bold"))
         content2_label.pack(pady = (10,0), padx = (10,0))
         
         mini_dashboard_content_frame3 = ctk.CTkFrame(mini_dashboard_container, fg_color = 'transparent')
-        mini_dashboard_content_frame3.pack(side = 'left', padx =20,fill = 'y')
+        mini_dashboard_content_frame3.pack(side = 'left', padx =24,fill = 'y')
         
         content3 = ctk.CTkFrame(mini_dashboard_content_frame3, height=100, width=285, corner_radius=20)
-        content3.pack(fill = 'y')
-        content3_label = ctk.CTkLabel(content3, text="No. of Users", height=100, width=285, anchor="nw", font= ctk.CTkFont('Arial', size = 22, weight = "bold"))
+        content3.pack()
+        content3.pack_propagate(0)
+        content3_label = ctk.CTkLabel(content3, text="No. of Users", height=80, width=260, anchor="nw", font= ctk.CTkFont('Arial', size = 22, weight = "bold"))
         content3_label.pack(pady = (10,0), padx = (10,0))
         
         #nav
@@ -309,7 +328,7 @@ class dashboard_window(ctk.CTk):
         nav_text = ctk.CTkLabel(nav_frame, text="Navigation",font= ctk.CTkFont('Arial', size = 22, weight = "bold"), text_color="black")
         nav_text.pack(side = 'left', pady = 15)
         
-        nav_container = ctk.CTkFrame(self, fg_color='transparent', width=self.winfo_width(), border_width=2, border_color='maroon')
+        nav_container = ctk.CTkFrame(self, fg_color='transparent', width=self.winfo_width(), border_width=2, border_color='maroon', corner_radius=10)
         nav_container.pack(fill = 'both', side = 'left', padx =15, pady = (0,20))
 
         #nav buttons
@@ -325,7 +344,7 @@ class dashboard_window(ctk.CTk):
         
         self.request_toplevel_window = None
         
-        report_icon = ctk.CTkImage(Image.open('python property transfer\images\icons8-file-100.png'), size=(90, 90))
+        report_icon = ctk.CTkImage(Image.open('python property transfer\images\icons8-file-128.png'), size=(90, 90))
         report_button = ctk.CTkButton(nav_container, command = self.open_report_toplevel, fg_color='transparent',image= report_icon, text='Reports', width= 30,text_color='black',compound="top")
         report_button.pack(side = 'left', pady = 10, padx = 45)
         
