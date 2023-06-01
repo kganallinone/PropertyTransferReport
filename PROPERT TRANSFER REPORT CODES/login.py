2b2d030f4b6288445c3687a7870a9c55eebc4b8b
K G A N <86733485+kganallinone@users.noreply.github.com>
2023-06-01 15:32:23 +0800

    Add files via upload

---
+++ PROPERT TRANSFER REPORT CODES/login.py
@@ -0,0 +1,194 @@
+from tkinter import *
+import customtkinter as ctk
+from PIL import ImageTk,Image
+import pyrebase
+from tkinter import messagebox
+
+
+def login_window():
+    from register import register_window
+    from page1_dashboard import dashboard_window
+    ctk.set_appearance_mode("light")  # Modes: system (default), light, dark
+    
+    #<----------------FIREBASE CONFIGURATION--------------------------------->#
+    config = {
+        "apiKey": "AIzaSyCyCtXg05ff-tocdUaxjBFfa550TLfKZQ4",
+        "authDomain": "puplopez-ptp.firebaseapp.com",
+        "projectId": "puplopez-ptp",
+        "databaseURL": "https://puplopez-ptp-default-rtdb.firebaseio.com/",
+        "storageBucket": "puplopez-ptp.appspot.com",
+        "messagingSenderId": "638312293451",
+        "appId": "1:638312293451:web:79e9405d11db0496f599ce",
+        "measurementId": "G-9Z3SPKFR9V"
+    }
+    #<----------------FIREBASE CONNECT--------------------------------->#
+    firebase = pyrebase.initialize_app(config)
+
+
+    # Get a reference to the auth service
+    auth = firebase.auth()
+
+    #<----------------FIREBASE END--------------------------------->#
+
+   
+    login = ctk.CTkToplevel()
+    login.title("Property Transfer | login")
+    login.geometry("{0}x{1}+200+50".format(1000, 600))
+    login.resizable(False, False)
+    login.configure(fg_color = 'white')
+    login.iconbitmap('PTR.ico')
+
+    #------------------- lOGIN FUNCTION ------------------------------------#
+    
+    def button_function():
+        email = login_email.get()
+        pw = login_pw.get()
+        
+        try:
+            auth.sign_in_with_email_and_password(email, pw)
+            login.withdraw()
+            dashboard_window()
+            
+        except:
+            login_pw.delete(0,len(pw))
+            answer = messagebox.askyesno("Question","Invalid user! Register first!")
+            print(answer)
+
+    
+    #------------------- DESIGN FUNCTION ------------------------------------#
+
+
+
+    #------------------- uI DESIGN------------------------------------#
+
+    login_openimg = Image.open(r'resources\images\signup_bg.png')
+    #resize_image = openimage1.resize(( login.winfo_width(),login.winfo_height()))
+    login_resize_image = login_openimg.resize((login.winfo_screenwidth(),login.winfo_screenheight()))
+    login_img1=ImageTk.PhotoImage(login_resize_image)
+    '''l1=ctk.CTkLabel(login,image=img1 )
+    l1.pack()'''
+    login_label= Label(login, image=login_img1)
+    login_label.image = login_img1
+    login_label.pack()
+    
+    
+   
+
+    
+    login_label1=ctk.CTkLabel( 
+        login_label, 
+        text="LOG-IN",
+        text_color='#3B3B3B',
+        font = ctk.CTkFont (
+        'Arial', 
+        weight="bold",
+        size= 40
+        ), 
+        fg_color='#FFFFFF'
+        )
+    login_label1.place(x=695, y=100)
+
+    login_email=ctk.CTkEntry(
+        login_label, 
+        width=320, 
+        height=40,
+        corner_radius= 30,
+        border_color='#C5C5C5',
+        placeholder_text='Email',
+        placeholder_text_color='#525252',
+        fg_color='#C5C5C5',
+        text_color='#3B3B3B'
+        )
+
+    login_email.place(x=610, y=170)
+
+
+    login_pw=ctk.CTkEntry(
+        login_label, 
+        width=320, 
+        height=40,
+        corner_radius= 30,
+        border_color='#C5C5C5',
+        placeholder_text='Password',
+        placeholder_text_color='#525252',
+        fg_color='#C5C5C5',
+        text_color='#3B3B3B',
+        show= '●'
+        )
+    login_pw.place(x=610, y=240)
+
+    login_label2=ctk.CTkLabel(
+        login_label, 
+        text="Forget Password?",
+        text_color='#525252',
+        font = ctk.CTkFont (
+        'Arial', 
+        weight="bold",
+        size= 12
+        ), 
+        fg_color='#FFFFFF'
+        )
+
+    login_label2.place(x=715, y=290)
+
+    #Create custom button
+    login_bttn1 = ctk.CTkButton(
+        login_label, 
+        command=button_function, 
+        width=150, 
+        height=40, 
+        compound="left", 
+        corner_radius= 30, 
+        fg_color='maroon', 
+        text_color='white', 
+        hover_color='#4F0000', 
+        font = ctk.CTkFont (
+            'Arial', 
+            weight="bold"
+            ), 
+        text="LOG-IN" 
+        )
+
+    login_bttn1.place(x=690, y=340)
+    
+    login_label3=ctk.CTkLabel(
+        login_label, 
+        text="Don't have an account yet?",
+        text_color='#525252',
+        font = ctk.CTkFont (
+        'Arial', 
+        weight="bold",
+        size= 12
+        ), 
+        fg_color='#FFFFFF'
+        )
+
+    login_label3.place(x=660, y=480) 
+    
+    def register_open():
+        login.withdraw()
+        register_window()
+        
+    login_label4=ctk.CTkLabel(
+        login_label,
+        text="Register.",
+        text_color='#0000FF',
+        font = ctk.CTkFont (
+        'Arial', 
+        weight="bold",
+        size= 12
+        ), 
+        fg_color='#FFFFFF'
+        )
+
+    login_label4.place(x=815, y=480)
+    login_label4.bind(
+        "<Button-1>", 
+        lambda e:
+        register_open()
+        )
+
+    login.mainloop()
+
+    
+#login_window()
---
+++ PROPERT TRANSFER REPORT CODES/page1_dashboard.py
@@ -0,0 +1,170 @@
+import tkinter as tk
+from tkinter import ttk
+import customtkinter as ctk
+from tkinter.messagebox import showinfo
+from PIL import Image
+import pyrebase
+import tkcalendar
+from ttkthemes import ThemedStyle
+        
+ctk.set_appearance_mode("light")  # Modes: system (default), light, dark       
+        
+def dashboard_window():      
+    from login import login_window
+    from page2_items import items_window
+    #<----------------FIREBASE CONFIGURATION--------------------------------->#
+    config = {
+        "apiKey": "AIzaSyCyCtXg05ff-tocdUaxjBFfa550TLfKZQ4",
+        "authDomain": "puplopez-ptp.firebaseapp.com",
+        "projectId": "puplopez-ptp",
+        "databaseURL": "https://puplopez-ptp-default-rtdb.firebaseio.com/",
+        "storageBucket": "puplopez-ptp.appspot.com",
+        "messagingSenderId": "638312293451",
+        "appId": "1:638312293451:web:79e9405d11db0496f599ce",
+        "measurementId": "G-9Z3SPKFR9V"
+    }
+    #<----------------FIREBASE CONNECT--------------------------------->#
+    firebase = pyrebase.initialize_app(config)
+
+    db = firebase.database()
+   
+    item_db = db.child("dashboard_info").child("ITEMNO").get()
+    item_total = item_db.val()['NO']
+
+    #<----------------FIREBASE END--------------------------------->#
+    dashboard = ctk.CTkToplevel()
+    dashboard.title("Property Transfer | Dashboard")
+    dashboard.geometry("{0}x{1}+200+50".format(1000,550))
+    dashboard.resizable(False, False)
+    dashboard.configure(fg_color = 'white')
+    dashboard.iconbitmap('resources\images\PTR.ico')
+
+    def items_open():
+        dashboard.withdraw()
+        items_window()
+        
+    def login_open():
+        dashboard.withdraw()
+        login_window()
+
+
+    #header frame
+    header_frame = ctk.CTkFrame(dashboard, fg_color='#313131', height=50, width=dashboard.winfo_width(), corner_radius=0)
+    header_frame.pack(fill = 'both', side='top')
+
+    #app logo
+    app_logo = ctk.CTkImage(Image.open('resources\images\DASHBOARD\PTRLogo_White.ico'), size=(25, 25))
+    app_logo_label = ctk.CTkLabel(header_frame, image= app_logo, text=None)
+    app_logo_label.pack(side = 'left', pady = 10, padx = 10)
+
+    #app name
+    app_name1 = ctk.CTkLabel(header_frame, text='PROPERTY', font= ctk.CTkFont('Arial', size = 22, weight = "bold"), text_color='white')
+    app_name1.pack(side = 'left', pady = 10, padx = 10)
+    app_name2 = ctk.CTkLabel(header_frame, text='TRANSFER', font= ctk.CTkFont('Arial', size = 22, weight = "bold"), text_color='#ee4444')
+    app_name2.pack(side = 'left', pady = 10)
+
+    
+    
+    logout_button = ctk.CTkButton(header_frame,command=login_open,  text='Logout', font=ctk.CTkFont('Arial', size=15), fg_color='#AC0D0D', text_color='white', hover_color='#880808', width= 30)
+    logout_button.pack(side = 'right', pady = 10, padx = 20)
+  
+    #settings
+    settings_icon = ctk.CTkImage(Image.open('resources\images\DASHBOARD\icons8-settings-50.png'), size=(30, 30))
+    settings_button = ctk.CTkButton(header_frame, image= settings_icon, text=None, fg_color='transparent', width= 30)
+    settings_button.pack(side = 'right', pady = 10, padx = 20)
+    
+    #user
+    user_icon = ctk.CTkImage(Image.open('resources\images\DASHBOARD\icons8-user-30.png'), size=(30, 30))
+    user_button = ctk.CTkButton(header_frame, image= user_icon, text=None, fg_color='transparent', width= 30)
+    user_button.pack(side = 'right', pady = 10, padx = 20)
+    
+    #footer
+    footer_frame = ctk.CTkFrame(dashboard, fg_color='maroon', height=26, corner_radius=0)
+    footer_frame.pack(fill = 'both', side='bottom')
+    
+    #mini dashboard frame
+    mini_dashboard_frame = ctk.CTkFrame(dashboard, fg_color='transparent')
+    mini_dashboard_frame.pack(fill='both')
+    
+    #mini dashboard
+    mini_dashboard_title = ctk.CTkLabel(mini_dashboard_frame, text="Mini Dashboard",font= ctk.CTkFont('Arial', size = 22, weight = "bold"), text_color="black")
+    mini_dashboard_title.pack(side = 'left', pady = 15, padx = 15)
+    
+    mini_dashboard_container = ctk.CTkFrame(dashboard, fg_color='transparent')
+    mini_dashboard_container.pack(fill = 'both')
+    
+    date_filter_icon = ctk.CTkImage(Image.open('resources\images\DASHBOARD\icons8-date-100.png'), size=(40, 40))
+    date_filter = ctk.CTkButton(mini_dashboard_frame, text =None, image=date_filter_icon,fg_color='transparent', width= 30 )
+    date_filter.pack(side = 'right', pady = 15, padx = 15)
+    
+    #mini dashboard contents
+    mini_dashboard_content_frame = ctk.CTkFrame(mini_dashboard_container, fg_color = 'transparent')
+    mini_dashboard_content_frame.pack(side = 'left', padx =24, fill = 'y')
+    
+    content1 = ctk.CTkFrame(mini_dashboard_content_frame, height=100, width=285, corner_radius=20)
+    content1.pack()
+    content1.pack_propagate(0)
+    content1_label = ctk.CTkLabel(content1, text="No. of Items",width=260, anchor="nw", font= ctk.CTkFont('Arial', size = 22, weight = "bold"), text_color='#3B3B3B')
+    content1_label.pack(pady = (4,0), padx = (0,0))
+    content1_label2 = ctk.CTkLabel(content1, text=item_total , width=260, anchor="e" ,font= ctk.CTkFont('Arial', size = 50, weight = "bold"), text_color='#525252')
+    content1_label2.pack(pady = (0,0), padx = (0,0))
+    
+    mini_dashboard_content_frame2 = ctk.CTkFrame(mini_dashboard_container, fg_color = 'transparent')
+    mini_dashboard_content_frame2.pack(side = 'left', padx =24, fill = 'y')
+    
+    content2 = ctk.CTkFrame(mini_dashboard_content_frame2, height=100, width=285, corner_radius=20 )
+    content2.pack()
+    content2.pack_propagate(0)
+    content2_label = ctk.CTkLabel(content2, text="No. of Transfers", height=80, width=260, anchor="nw", font= ctk.CTkFont('Arial', size = 22, weight = "bold"))
+    content2_label.pack(pady = (10,0), padx = (10,0))
+    
+    mini_dashboard_content_frame3 = ctk.CTkFrame(mini_dashboard_container, fg_color = 'transparent')
+    mini_dashboard_content_frame3.pack(side = 'left', padx =24,fill = 'y')
+    
+    content3 = ctk.CTkFrame(mini_dashboard_content_frame3, height=100, width=285, corner_radius=20)
+    content3.pack()
+    content3.pack_propagate(0)
+    content3_label = ctk.CTkLabel(content3, text="No. of Users", height=80, width=260, anchor="nw", font= ctk.CTkFont('Arial', size = 22, weight = "bold"))
+    content3_label.pack(pady = (10,0), padx = (10,0))
+    
+    #nav
+    nav_frame = ctk.CTkFrame(mini_dashboard_content_frame, fg_color='transparent')
+    nav_frame.pack(fill = 'both')
+    
+    nav_text = ctk.CTkLabel(nav_frame, text="Navigation",font= ctk.CTkFont('Arial', size = 22, weight = "bold"), text_color="black")
+    nav_text.pack(side = 'left', pady = 15)
+    
+    nav_container = ctk.CTkFrame(dashboard, fg_color='transparent', width=dashboard.winfo_width(), border_width=2, border_color='maroon', corner_radius=10)
+    nav_container.pack(fill = 'both', side = 'left', padx =15, pady = (0,20))
+
+    #nav buttons
+    items_icon = ctk.CTkImage(Image.open('resources\images\DASHBOARD\icons8-item-90.png'), size=(90, 90))
+    items_button = ctk.CTkButton(nav_container, command=items_open, fg_color='transparent', image= items_icon, text='Items', width= 30, text_color='black', compound="top")
+    items_button.pack(side = 'left', pady = 10, padx = 45)
+    
+
+    
+    request_icon = ctk.CTkImage(Image.open('resources\images\DASHBOARD\icons8-table-100.png'), size=(90, 90))
+    request_button = ctk.CTkButton(nav_container, fg_color='transparent', image= request_icon, text='Requests', width= 30, text_color='black', compound="top")
+    request_button.pack(side = 'left', pady = 10, padx = 45)
+    
+
+    
+    report_icon = ctk.CTkImage(Image.open('resources\images\DASHBOARD\icons8-file-128.png'), size=(90, 90))
+    report_button = ctk.CTkButton(nav_container, fg_color='transparent',image= report_icon, text='Reports', width= 30,text_color='black',compound="top")
+    report_button.pack(side = 'left', pady = 10, padx = 45)
+    
+
+    
+    users_icon = ctk.CTkImage(Image.open('resources\images\DASHBOARD\icons8-users-90.png'), size=(90, 90))
+    users_button = ctk.CTkButton(nav_container, fg_color='transparent', image= users_icon, text='Users', width= 30,text_color='black', compound="top")
+    users_button.pack(side = 'left', pady = 10, padx = 45)
+
+    
+    history_icon = ctk.CTkImage(Image.open('resources\images\DASHBOARD\icons8-history-96.png'), size=(90, 90))
+    history_button = ctk.CTkButton(nav_container,fg_color='transparent', image= history_icon, text='History', width= 30, text_color='black',compound="top")
+    history_button.pack(side = 'left', pady = 10, padx = 45)
+    
+    dashboard.mainloop()    
+    
+dashboard_window()
