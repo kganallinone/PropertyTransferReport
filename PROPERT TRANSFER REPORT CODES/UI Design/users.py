c7ed7a49e1e0994b754112098c50253237d913f6
Argel <107836860+MarkMalihan@users.noreply.github.com>
2023-06-08 16:23:22 +0800

    Update users.py

--- PROPERT TRANSFER REPORT CODES/UI Design/users.py
+++ PROPERT TRANSFER REPORT CODES/UI Design/users.py
@@ -26,7 +26,7 @@ def user_list_window():
     
     user_list = ctk.CTk()
     user_list.title("Property Transfer | Users")
-    user_list.geometry("{0}x{1}+250+50".format(950,500))
+    user_list.geometry("{0}x{1}+250+50".format(935,500))
     user_list.resizable(False, False)
     user_list.configure(fg_color = 'white')
     user_list.iconbitmap('python property transfer\images\icons8-data-transfer-483.ico')
@@ -54,43 +54,30 @@ def user_list_window():
     frame1 = ctk.CTkFrame(user_list, fg_color='transparent')
     frame1.pack(side = 'left', fill = 'both', pady = 10, padx = 15)
     
-    columns = ('id_number', 'lastname', 'firstname', 'email' ,'usertype')
+    columns = ('id_number', 'lastname', 'firstname', 'email' ,'usertype', 'department')
     
     user_table = ttk.Treeview(frame1, columns=columns, show='headings', height=20)
     user_table.pack(fill=tk.BOTH)
     
-    user_table.column('id_number', width=150)
-    user_table.column('lastname', width=150)
-    user_table.column('firstname', width=150)
-    user_table.column('email', width=150)
-    user_table.column('usertype', width=150)
+    user_table.column('id_number', width=150, anchor='center')
+    user_table.column('lastname', width=150, anchor='center')
+    user_table.column('firstname', width=150, anchor='center')
+    user_table.column('email', width=150, anchor='center')
+    user_table.column('usertype', width=150, anchor='center')
+    user_table.column('department', width=150, anchor='center')
     
     user_table.heading('id_number', text='ID Number')
     user_table.heading('lastname', text='Last Name')
     user_table.heading('firstname', text='First Name')
     user_table.heading('email', text='Email')
     user_table.heading('usertype', text='User Type')
+    user_table.heading('department', text='Department')
     
     user_db = db.child("user_accounts").order_by_child("ID").get()
     for dt in user_db.each():
-        user_table.insert("",'end',values=(dt.val()['ID'],dt.val()['LN'],dt.val()['FN'],dt.val()['EMAIL'],dt.val()['TYPE']))
+        user_table.insert("",'end',values=(dt.val()['ID'],dt.val()['LN'],dt.val()['FN'],dt.val()['EMAIL'],dt.val()['TYPE'],dt.val()['DEPARTMENT']))
     
-    #Delete User
-    frame2 = ctk.CTkFrame(user_list, fg_color='transparent')
-    frame2.pack(side = 'right', fill = 'both' ,pady = 15, padx = (0,15))
-    
-    delete_frame = ctk.CTkFrame(frame2, fg_color='#E9E9E9')
-    delete_frame.pack()
-    
-    del_label = ctk.CTkLabel(delete_frame, text='Delete User')
-    del_label.pack(pady = 10, padx = 15)
-    
-    del_entry = ctk.CTkEntry(delete_frame, placeholder_text='Enter User ID', width=150)
-    del_entry.pack(pady = 10, padx = 15)
-    
-    del_button = ctk.CTkButton(delete_frame, text='Delete', width= 100,fg_color='#0077B8', text_color='white')
-    del_button.pack(pady = 10)
-    
+
     user_list.mainloop()
 
 
